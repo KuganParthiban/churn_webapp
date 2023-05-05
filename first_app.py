5ebe1da98dff9615eccc0b481ebfbe177914c822
KuganParthiban <121218920+KuganParthiban@users.noreply.github.com>
2023-05-06 03:19:11 +0800

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -42,24 +42,77 @@ def main():
     
     #getting the input data from the user
     
-    Phone_Service = st.text_input('Do you take any mobile services?')
-    Multiple_Line = st.text_input('Do you own multiple lines?')
-    Internet_Service = st.text_input('Got internet service?')
-    Online_Security = st.text_input('Got online security?')
-    Online_Backup = st.text_input('Got online backup?')
-    Device_Protection = st.text_input('Got device protection?')
-    Tech_Support = st.text_input('Got tech support?')
-    Streaming_TV = st.text_input('Got StreamingTV?')
-    Streaming_Movies = st.text_input('Got StreamingMovies?')
-    Contract = st.text_input('Contract?')
-    Paperless_Billing = st.text_input('PaperlessBilling?')
-    Payment_Method = st.text_input('PaymentMethod?')
-    Monthly_Charges = st.text_input('MonthlyCharges?')
-    Total_Charges = st.text_input('TotalCharges?')
-    Gender = st.text_input('Gender?')
-    Senior_Citizen = st.text_input('Are you a senior citizen?')
-    Partner = st.text_input('Got Partner?')
-    Dependents = st.text_input('Got Dependents?')
+    MonthlyCharges = st.text_input('MonthlyCharges How Much?')
+    
+    TotalCharges = st.text_input('TotalCharges how much?')
+    
+    Phone_Service_Yes = st.text_input('Phone_Service_Yes? If yes answer 1 else 0')
+    
+    Multiple_Line_Nophoneservice = st.text_input('Multiple_Line_No phone service?if no answer 1 else 0')
+    
+    Multiple_Line_Yes = st.text_input('Got online backup?0 or 1')
+    
+    
+    Internet_Service_Fiberoptic = st.text_input('Internet_Service_Fiber optic?0 or 1')
+    
+    Internet_Service_No = st.text_input('Internet_Service_No? no is 1 yes 0')
+    
+    Online_Security_Nointernetservice = st.text_input('Online_Security_No internet service?no is 1 yes 0')
+    
+    Online_Security_Yes = st.text_input('Online_Security_Yes? yes 1 no 0')
+        
+        
+    Online_Backup_No_internet_service = st.text_input('Online_Backup_No internet service?no is 1')
+    
+    Online_Backup_Yes = st.text_input('Online_Backup_Yes?yes is 1')
+    
+    DeviceProtection_No_internet_service = st.text_input('DeviceProtection_No internet service?no is 1')
+    
+    DeviceProtection_Yes = st.text_input('DeviceProtection_Yes? yes 1 else 0')
+    
+    TechSupport_No_internet_service = st.text_input('TechSupport_No internet service?')
+    
+        
+    TechSupport_Yes = st.text_input('TechSupport_Yes?')
+    
+    StreamingTV_No_internet_service = st.text_input('StreamingTV_No internet service?')
+    
+    StreamingTV_Yes = st.text_input('StreamingTV_Yes?')
+    
+    StreamingMovies_Yes = st.text_input('StreamingMovies_Yes?')
+    
+    StreamingMovies_Nointernetservice = st.text_input('StreamingMovies_No internet service?')
+            
+    Contract_Oneyear = st.text_input('Contract_One year? if yes 1 no 0')
+    
+    Contract_Twoyear = st.text_input('Contract_Two year?')
+    
+    PaperlessBilling_Yes = st.text_input('PaperlessBilling_Yes?')
+    
+    PaymentMethod_Creditcard_automatic = st.text_input('PaymentMethod_Creditcardautomati?')
+    
+    PaymentMethod_Electroniccheck = st.text_input('PaymentMethod_Electroniccheck?')
+      
+    PaymentMethod_Mailedcheck = st.text_input('tPaymentMethod_Mailedcheck?')
+    
+    Gender_Male = st.text_input('Gender_Male?if male 1 else 0')
+    
+    Senior_Citizen_1 = st.text_input('Senior_Citizen_1?')
+    
+    Partner_Yes = st.text_input('Partner_Yes?')
+    
+    Dependents_Yes = st.text_input('Dependents_Yes?')
+    
+    tenure_group_13_24 = st.text_input('tenure_group_13 - 24?')
+    
+    tenure_group_25_36 = st.text_input('tenure_group_25 - 36?')
+    
+    tenure_group_37_48 = st.text_input('tenure_group_37 - 48?')
+    
+    tenure_group_49_60 = st.text_input('tenure_group_49 - 60?')
+    
+    tenure_group_61_72  = st.text_input('tenure_group_61 - 72 ?')
+    
     
 
     
@@ -72,12 +125,20 @@ def main():
     # creating a button for prediction
     
     if st.button('Churn Result'):
-        diagnosis = churn_prediction([Phone_Service,Multiple_Line,Internet_Service,
-                                     Online_Security, Online_Backup, Device_Protection,
-                                     Tech_Support,Streaming_TV,Streaming_Movies,
-                                     Contract,Paperless_Billing,Payment_Method,
-                                     Monthly_Charges,Total_Charges,Gender,
-                                     Senior_Citizen,Partner,Dependents])
+        diagnosis = churn_prediction([MonthlyCharges, TotalCharges, Phone_Service_Yes,Multiple_Line_Nophone_service,
+                                      Multiple_Line_Yes,Internet_Service_Fiber_optic,Internet_Service_No,
+                                      Online_Security_Nointernetservice, Online_Security_Yes,
+                                      Online_Backup_Nointernetservice, Online_Backup_Yes,
+                                      DeviceProtection_Nointernetservice, DeviceProtection_Yes,
+                                      TechSupport_Nointernetservice, TechSupport_Yes,
+                                      StreamingTV_Nointernetservice, StreamingTV_Yes,
+                                      StreamingMovies_Nointernetservice, StreamingMovies_Yes,
+                                      Contract_One_year, Contract_Twoyear, PaperlessBilling_Yes,
+                                      PaymentMethod_Creditcard_automatic,
+                                      PaymentMethod_Electronicheck, PaymentMethod_Mailedcheck,
+                                      Gender_Male, Senior_Citizen_1, Partner_Yes, Dependents_Yes,
+                                      tenure_group_13_24, tenure_group_25_36, tenure_group_37_48,
+                                      tenure_group_49_60, tenure_group_61_72])
     
     st.success(diagnosis)
     
