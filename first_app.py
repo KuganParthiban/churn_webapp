532808ba5a2719417585dc7ecf6c1de9bd6c7ced
KuganParthiban <kugan.parthiban@gmail.com>
2023-05-29 13:56:31 +0800

    update

--- first_app.py
+++ first_app.py
@@ -8,42 +8,39 @@
 import numpy as np
 import pickle
 import streamlit as st
+from sklearn.preprocessing import LabelEncoder
 
 #loading the model
 loaded_model = pickle.load(open('churn_predict.pkl','rb'))
 
-
 # creating a function for prediction
-
 def churn_prediction(input_data):
-    
-    # changing the input_data to numpy array
+    # Convert categorical features using Label Encoding
+    categorical_features = ['PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'Gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenuregroup']
+    label_encoder = LabelEncoder()
+    for feature in categorical_features:
+        input_data[feature] = label_encoder.transform([input_data[feature]])
+
+    # Convert input data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    # Reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
 
-    if (prediction[0] == 0):
-      return 'The person is not churn'
+    if prediction[0] == 0:
+        return 'The person is not churn'
     else:
-      return 'The person is churn'
-    
-
+        return 'The person is churn'
 
 def main():
-    
-    
-    #giving a title 
+    # giving a title
     st.title('Churn Prediction Web App')
-    
-    
-    #getting the input data from the user
-    
+
+    # getting the input data from the user
     PhoneService = st.selectbox('Do you have phone service?', ('Yes', 'No'), key="phone_service_key")
-    MultipleLine = st.selectbox('Do you have multiple lines?', ('Yes', 'No', 'No phone service'), key="multiple_line_key")
+    MultipleLines = st.selectbox('Do you have multiple lines?', ('Yes', 'No', 'No phone service'), key="multiple_line_key")
     InternetService = st.selectbox('Got internet service?', ('DSL', 'Fiber Optic', 'No'), key="internet_service_key")
     OnlineSecurity = st.selectbox('Got online security?', ('Yes', 'No', 'No internet service'), key="online_security_key")
     OnlineBackup = st.selectbox('Got online backup?', ('Yes', 'No', 'No internet service'), key="online_backup_key")
@@ -59,43 +56,39 @@ def main():
     Partner = st.selectbox('Do you have a partner?', ('Yes', 'No'), key="partner_key")
     Dependents = st.selectbox('Do you have any dependents?', ('Yes', 'No'), key="dependents_key")
     tenuregroup = st.selectbox('How long is your tenure?', ('1 - 12', '25 - 36', '37 - 48', '13 - 24', '61 - 72', '49 - 60'), key="tenure_group_key")
-    MonthlyCharges = st.sidebar.slider('What is your monthly charges?', 18,119,70)
-    TotalCharges = st.sidebar.slider('What is your total charges?', 18,8685,1397)
-    
+    MonthlyCharges = st.sidebar.slider('What is your monthly charges?', 18, 119, 70)
+    TotalCharges = st.sidebar.slider('What is your total charges?', 18, 8685, 1397)
 
-    
     # code for prediction
-    
     diagnosis = ''
-    
-    
+
     # creating a button for prediction
-    
     if st.button('Churn Result'):
-        diagnosis = churn_prediction(['Phone Service',
- 'MultipleLine',
- 'InternetService',
- 'OnlineSecurity',
- 'OnlineBackup',
- 'DeviceProtection',
- 'TechSupport',
- 'StreamingTV',
- 'StreamingMovies',
- 'Contract',
- 'PaperlessBilling',
- 'PaymentMethod',
- 'MonthlyCharges',
- 'TotalCharges',
- 'Gender',
- 'SeniorCitizen',
- 'Partner',
- 'Dependents',
- 'tenuregroup'])
-    
+        input_data = {
+            'PhoneService': PhoneService,
+            'MultipleLines': MultipleLines,
+            'InternetService': InternetService,
+            'OnlineSecurity': OnlineSecurity,
+            'OnlineBackup': OnlineBackup,
+            'DeviceProtection': DeviceProtection,
+            'TechSupport': TechSupport,
+            'StreamingTV': StreamingTV,
+            'StreamingMovies': StreamingMovies,
+            'Contract': Contract,
+            'PaperlessBilling': PaperlessBilling,
+            'PaymentMethod': PaymentMethod,
+            'Gender': Gender,
+            'SeniorCitizen': SeniorCitizen,
+            'Partner': Partner,
+            'Dependents': Dependents,
+            'tenuregroup': tenuregroup,
+            'MonthlyCharges': MonthlyCharges,
+            'TotalCharges': TotalCharges
+        }
+        diagnosis = churn_prediction(input_data)
+
     st.success(diagnosis)
-    
-    
-    
+
 if __name__ == '__main__':
     main()
 
